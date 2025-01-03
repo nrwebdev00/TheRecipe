2f2613d6babacbfd770406afaf39e44ffde393f4
Nathon Reed <nrwebdev00@gmail.com>
2025-01-03 07:39:37 -0800

    Recipe model created - basic Recipe

--- Recipe/admin.py
+++ Recipe/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'author', 'created',)
+    list_filter = ('created','status')
+    search_fields = ('title','status')
+    raw_id_fields =['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'created', 'title']
+    show_facets = admin.ShowFacets.ALWAYS
--- Recipe/models.py
+++ Recipe/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+# Managers
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return (
+            super().get_queryset().filter(status=Recipe.Status.PUBLISHED)
+        )
+
+class FlagManager(models.Manager):
+    def get_queryset(self):
+        return(
+            super().get_queryset().filter(status=Recipe.Status.FLAGGED)
+        )
+
+class AdminFlagManager(models.Manager):
+    def get_queryset(self):
+        return(
+            super().get_queryset().filter(status=Recipe.Status.ADMIN_FLAGGED)
+        )
+
+
+class Recipe(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF','Draft'
+        PUBLISHED = 'PUB','Published'
+        FLAGGED = 'FG','Flagged'
+        ADMIN_FLAGGED = 'AFG','AdminFlagged'
+
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='recipes')
+    slug = models.SlugField(max_length=255)
+    ingredient = models.TextField()
+    directions = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=25, choices=Status, default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishManager()
+    flagged = FlagManager()
+    adminFlagged = AdminFlagManager()
+
+
+    class Meta:
+        ordering = ['-publish', 'title']
+        indexes = [
+            models.Index(fields=['-publish', 'title']),
+        ]
+
+    def __str__(self):
+        return self.title
